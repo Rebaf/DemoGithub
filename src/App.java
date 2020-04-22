6b037973d531fd29dce56e912040fc6f70cdfd50
Faber C (Cor) <cor.faber@intraffic.nl>
2020-04-22 15:38:24 +0200

    Add some cool stuff

--- src/App.java
+++ src/App.java
@@ -2,4 +2,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Mijn App");
     }
+
+    private void doStuff() {
+        System.out.println("Doing a lot of cool Stuff....");
+    }
 }
