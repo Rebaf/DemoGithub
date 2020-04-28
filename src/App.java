7354f9d7e197228ca4d6e22d39a3d5062a873787
Rebaf <cor.faber@additude.eu>
2020-04-28 12:11:42 +0200

    reset

--- src/App.java
+++ src/App.java
@@ -4,14 +4,6 @@ public class App {
     }
 
     private static void doStuff() {
-        System.out.println("App.doStuff called by featureY & featureZ");
-    }
-
-    private static void featureY() {
-        doStuff();
-    }
-
-    private static void featureZ() {
-        doStuff();
+        System.out.println("App.doStuff");
     }
 }
